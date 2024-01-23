26ee2322a0ba1436033c7bc6dde1e2f3bfd4ef18
Georg Niggli <jornig@outlook.com>
2024-01-23 23:25:53 +0100

    init

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Computer Science Assignment
+
+Task: Determine Email Legitimacy
+
+Recently, I watched a video about how autocorrect works, and I thought that the same principle could be applied to spam detection. In my experience, most spam emails involve phishing attempts, where they try to trick users into clicking on malicious links.
+
+My approach is to calculate the Levenshtein distance for each word against a list of popular URLs. If the distance is not zero but also not greater than 3, it is highly likely that the URL is attempting to deceive people.
+
+I haven't conducted extensive research beyond understanding how the algorithm works, so I'm unsure if this approach is a common practice in spam detection.
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+fn min(a: usize, b: usize, c: usize) -> usize {
+    usize::min(a, usize::min(b, c))
+}
+
+fn levenshtein_distance(s1: &str, s2: &str) -> usize {
+    let len1 = s1.chars().count();
+    let len2 = s2.chars().count();
+
+    let mut matrix = vec![vec![0; len2 + 1]; len1 + 1];
+
+    for i in 0..=len1 {
+        matrix[i][0] = i;
+    }
+
+    for j in 0..=len2 {
+        matrix[0][j] = j;
+    }
+
+    for (i, char1) in s1.chars().enumerate() {
+        for (j, char2) in s2.chars().enumerate() {
+            let cost = if char1 == char2 { 0 } else { 1 };
+
+            matrix[i + 1][j + 1] = min(
+                matrix[i][j + 1] + 1,
+                matrix[i + 1][j] + 1,
+                matrix[i][j] + cost,
+            );
+        }
+    }
+
+    matrix[len1][len2]
+}
+
+fn main() {
+    let s1 = "google";
+    let s2 = "googol";
+    let distance = levenshtein_distance(s1, s2);
+    println!("Levenshtein distance between '{}' and '{}' is: {}", s1, s2, distance);
+}
