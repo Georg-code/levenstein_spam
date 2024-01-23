c99b8d670b965e9eeeba740b521477184b9e074e
Georg Niggli <jornig@outlook.com>
2024-01-23 23:56:16 +0100

    added brands

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod get_brands;
 mod levenstein_distance;
-
+pub use get_brands::*;
 pub use levenstein_distance::*;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use levenstein_spam::get_brands;
 use levenstein_spam::levenshtein_distance;
 
 use std::io;
@@ -6,13 +7,20 @@ fn main() {
     // Read input text
     let mut input_text = String::new();
     println!("Enter a long text:");
-    io::stdin().read_line(&mut input_text).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("Failed to read line");
 
     // Trim whitespaces
-    let input_text = input_text.trim();
+    for word in input_text.split_whitespace() {
+        for brand in get_brands() {
+            let distance = levenshtein_distance(brand, word);
+
+            if distance <= 2 && distance != 0 {
+                println!("{} could be a spam attack on {}", word, brand);
+            }
+        }
+    }
 
     // Example comparison
-    let s1 = "kitten";
-    let distance = levenshtein_distance(s1, input_text);
-    println!("Levenshtein distance between '{}' and entered text is: {}", s1, distance);
 }
