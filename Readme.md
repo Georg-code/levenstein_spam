697b901b11574393a0d808d2dd209f82268bf6d4
Georg Niggli <jornig@outlook.com>
2024-01-24 01:05:05 +0100

    cleaned up

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
-# Computer Science Assignment
+# Computer Science Homework (EF-KUE)
 
 Task: Determine Email Legitimacy
 
@@ -6,4 +6,4 @@ Recently, I watched a video about how autocorrect works, and I thought that the 
 
 My approach is to calculate the Levenshtein distance for each word against a list of popular URLs. If the distance is not zero but also not greater than 3, it is highly likely that the URL is attempting to deceive people.
 
-I haven't conducted extensive research beyond understanding how the algorithm works, so I'm unsure if this approach is a common practice in spam detection.
+I haven't conducted extensive research beyond understanding how to implement the algorithem, so I'm no sure if this approach is a common practice in spam detection.
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,12 @@ use levenstein_spam::levenshtein_distance;
 use std::io;
 
 fn main() {
-    // Read input text
     let mut input_text = String::new();
     println!("Enter a long text:");
     io::stdin()
         .read_line(&mut input_text)
         .expect("Failed to read line");
 
-    // Define the regular expression
     let re = Regex::new(
         r"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)",
     )
@@ -22,7 +20,6 @@ fn main() {
     for mat in re.find_iter(input_text.as_str()) {
         domain_bodies.push(mat.unwrap().as_str());
     }
-    println!("{:?}", domain_bodies);
 
     let top_level =
         Regex::new(r"(?:[^.]+)\.(gov\.uk|ac\.uk|co\.uk|com|org|net|us|uk|ch|eu|de)(?:\/|$)")
@@ -31,20 +28,17 @@ fn main() {
     let mut domains = Vec::new();
     for domain_body in domain_bodies {
         for mat in top_level.find_iter(domain_body) {
-            // split mat at the first dot and take the first part
-
             let sld = mat.as_ref().unwrap().as_str().split(".").next().unwrap();
             domains.push(sld);
         }
     }
-    println!("{:?}", domains);
 
-    for word in input_text.split_whitespace() {
+    for sld in domains {
         for brand in get_brands() {
-            let distance = levenshtein_distance(brand, word);
+            let distance = levenshtein_distance(brand, sld);
 
             if distance <= 2 && distance != 0 {
-                println!("{} could be a spam attack on {}", word, brand);
+                println!("{} could be a spam attack on {}", sld, brand);
             }
         }
     }
